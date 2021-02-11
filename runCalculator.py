91929e8bb4f16192452271ebcdcb8d2feb32b7f3
Vilma <alab.vilma@gmail.com>
2021-02-11 15:14:21 +0100

    GUI

--- runCalculator.py
+++ runCalculator.py
@@ -6,6 +6,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.uic import *
 from PyQt5.QtWidgets import *
+from datetime import datetime
 from parcelServiceOption import ParcelService, ParcelOption, Parcel
 
 SERVICES_FILE = "services.json"
@@ -22,31 +23,60 @@ def get_available_services():
     return parcel_services
 
 def calculate():
-    #TODO convert to int in nicer way
     parcel = Parcel([int(w.inp_length.text()), int(w.inp_width.text()), int(w.inp_height.text())], int(w.inp_weight.text()))
     for service in parcel_services:
         service.add_best_option(parcel)          
     show_best_options(parcel)
 
+
 def show_best_options(parcel):
-    for i in parcel.best_parcel_options:
-        price_formatted = str(Decimal(i.price).quantize(Decimal('.01'), rounding=ROUND_UP))
-        # TODO send to vk_rechner_main      
-        print(f"{i.ps_name:20}| {i.name:20}| {price_formatted:>10} €| {i.delivery_date}")
+    pw.display_length.setText(str(parcel.sizes[0]))
+    pw.display_width.setText(str(parcel.sizes[1]))
+    pw.display_height.setText(str(parcel.sizes[2]))
+    pw.display_weight.setText(str(parcel.weight))
+    
+    pw.tableWidget.setRowCount(len(parcel.best_parcel_options))                                 # Zeilenzahl = anzahl Anbieter + überschrift
+    pw.tableWidget.setColumnCount(4)                              # sollten fest 4 Spalten sein
+
+    # Überschrift (erste Zeile row0
+    item = QTableWidgetItem()
+    item.setText("Anbieter")
+    pw.tableWidget.setHorizontalHeaderItem(0, item)
+    item = QTableWidgetItem()
+    item.setText("Produkt")
+    pw.tableWidget.setHorizontalHeaderItem(1, item)
+    item = QTableWidgetItem()
+    item.setText("Preis, €")
+    pw.tableWidget.setHorizontalHeaderItem(2, item)
+    item = QTableWidgetItem()
+    item.setText("voraussichtliches Lieferdatum")
+    pw.tableWidget.setHorizontalHeaderItem(3, item)
+    
+    # pw.tableWidget.setItem(0,0, QTableWidgetItem("Anbieter"))
+    # pw.tableWidget.setItem(0,1, QTableWidgetItem("Produkt"))
+    # pw.tableWidget.setItem(0,2, QTableWidgetItem("Preis"))
+    # pw.tableWidget.setItem(0,3, QTableWidgetItem("voraussichtliches Lieferdatum"))
+   
+    for i in range(len(parcel.best_parcel_options)):
+    #for i in parcel.best_parcel_options:
+        option = parcel.best_parcel_options[i]
+        price_formatted = str(Decimal(option.price).quantize(Decimal('.01'), rounding=ROUND_UP))
+        #print(f"{i.ps_name:20}| {i.name:20}| {price_formatted:>10} €| {i.delivery_date}")
+        pw.tableWidget.setItem(i,0, QTableWidgetItem(option.ps_name))
+        pw.tableWidget.setItem(i,1, QTableWidgetItem(option.name))
+        pw.tableWidget.setItem(i,2, QTableWidgetItem(str(price_formatted)))
+        	
+        pw.tableWidget.setItem(i,3, QTableWidgetItem(option.delivery_date.strftime("%d.%m.%Y")))
+    pw.show()    
+
         
 def show_prices(parcel_service):
     print("Here open window with all available services")
     for p in parcel_services:
         print(p)
 
-def alternate():
-    print("TODO")
-
 def close():
     w.destroy()
-    
-def show_help_w():
-    print("TODO")
 
 if __name__ == '__main__':
     parcel_services = get_available_services()
@@ -56,10 +86,11 @@ if __name__ == '__main__':
     w = loadUi("vk_rechner_main.ui")            # Laden des Hauptfenster (main)
     w.but_calculate.clicked.connect(calculate)
     w.but_pricelist.clicked.connect(show_prices)
-    w.but_alternate.clicked.connect(alternate)
-    w.but_close.clicked.connect(show_help_w)
     w.but_close.clicked.connect(close)
     w.show()                                    # Anzeige des Fensters
+    
+    pw = loadUi("display_prices.ui") 
+    
 
     sys.exit(app.exec_())                       # Applikation starten
 
